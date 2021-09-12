a884fd49a975a924856e0b3f055c0a55f3ddbf7f
Rob Wilson <rob@rwilson.org.uk>
2021-09-12 10:44:05 +0100

    single var for inputs

--- main.ts
+++ main.ts
@@ -4,19 +4,24 @@ datalogger.onLogFull(function () {
 input.onButtonPressed(Button.AB, function () {
     datalogger.deleteLog()
 })
-let P1State = 0
+let inNumber = 0
 let logging = false
 logging = true
-datalogger.setColumns(["P0", "P1"])
+datalogger.setColumns(["inNumber"])
 datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
-loops.everyInterval(10000, function () {
+datalogger.mirrorToSerial(true)
+loops.everyInterval(1000, function () {
+    inNumber = 0
     if (logging) {
-        P1State = pins.digitalReadPin(DigitalPin.P1)
-        if (P1State == 0) {
-            basic.showIcon(IconNames.No)
-        } else {
-            basic.showIcon(IconNames.Yes)
-        }
-        datalogger.logData([datalogger.createCV("P1", P1State)])
+        inNumber += pins.digitalReadPin(DigitalPin.P1)
+        inNumber += 2 * pins.digitalReadPin(DigitalPin.P2)
+        inNumber += 4 * pins.digitalReadPin(DigitalPin.P3)
+        inNumber += 8 * pins.digitalReadPin(DigitalPin.P4)
+        inNumber += 16 * pins.digitalReadPin(DigitalPin.P5)
+        inNumber += 32 * pins.digitalReadPin(DigitalPin.P6)
+        inNumber += 64 * pins.digitalReadPin(DigitalPin.P7)
+        inNumber += 128 * pins.digitalReadPin(DigitalPin.P8)
+        inNumber += 256 * pins.digitalReadPin(DigitalPin.P9)
+        datalogger.logData([datalogger.createCV("inNumber", inNumber)])
     }
 })
