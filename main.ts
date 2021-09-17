98d2428c8c13e447115c46e89b2b11e33ac1dc67
Rob Wilson <rob@rwilson.org.uk>
2021-09-17 09:21:48 +0100

    added upload for BT

--- main.ts
+++ main.ts
@@ -13,6 +13,9 @@ function parseCommand () {
         serial.writeLine("Invalid command")
     }
 }
+function dec2bin (num: number) {
+    return "1011"
+}
 function leadingZero (num: number) {
     if (num < 10) {
         return "0" + num
@@ -21,7 +24,31 @@ function leadingZero (num: number) {
     }
 }
 function upLoad () {
-	
+    basic.pause(100)
+    readingsLength = dateTimeReadings.length
+    if (readingsLength != 0) {
+        for (let index = 0; index <= readingsLength - 1; index++) {
+            let connected = 0
+            if (connected == 1) {
+                pinReading = dec2bin(pinReadings[index])
+                bluetooth.uartWriteString(dateTimeReadings[index])
+                basic.pause(100)
+                bluetooth.uartWriteString(",")
+                basic.pause(100)
+                for (let bit = 0; bit <= 4; bit++) {
+                    bluetooth.uartWriteString("" + pinReading.charAt(8 - bit) + ",")
+                }
+                for (let bit = 0; bit <= 3; bit++) {
+                    bluetooth.uartWriteString("" + pinReading.charAt(3 - bit) + ",")
+                }
+                bluetooth.uartWriteLine("")
+                basic.pause(100)
+            }
+        }
+        basic.showIcon(IconNames.Yes)
+    } else {
+        bluetooth.uartWriteLine("No stored readings!")
+    }
 }
 function dateTimeString () {
     return "" + leadingZero(DS3231.date()) + "/" + leadingZero(DS3231.month()) + "/" + DS3231.year() + " " + leadingZero(DS3231.hour()) + ":" + leadingZero(DS3231.minute())
@@ -45,15 +72,19 @@ function setTime () {
     serial.writeLine("Date & time have been set ")
 }
 let inNumber = 0
-let charIn: string = []
-let mm: string = []
-let hh: string = []
-let dt: string = []
-let mo: string = []
-let yr: string = []
-let params: string = []
-let stringIn: string = []
-let command: string = []
+let charIn = ""
+let mm = ""
+let hh = ""
+let dt = ""
+let mo = ""
+let yr = ""
+let pinReading = ""
+let readingsLength = 0
+let params = ""
+let stringIn = ""
+let command = ""
+let pinReadings: number[] = []
+let dateTimeReadings: string[] = []
 let Pstate = [0]
 let sampleTime = 5000
 pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
@@ -68,8 +99,8 @@ pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P9, PinPullMode.PullNone)
 let logging = true
 let lastInNumber = 1024
-let dateTimeReadings: number[] = []
-let pinReadings = [0]
+dateTimeReadings = [""]
+pinReadings = [0]
 basic.forever(function () {
     charIn = serial.readString()
     stringIn = "" + stringIn + charIn
@@ -78,7 +109,7 @@ basic.forever(function () {
         parseCommand()
         stringIn = ""
     } else {
-        serial.writeString("" + (charIn))
+        serial.writeString(charIn)
     }
 })
 loops.everyInterval(sampleTime, function () {
