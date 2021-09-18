347d1a22ab1861b6ec2c315dd60c217e3bcdae50
Rob Wilson <rob@rwilson.org.uk>
2021-09-18 10:05:43 +0100

    fixed uploadUSB, added sceonds

--- main.ts
+++ main.ts
@@ -9,11 +9,14 @@ function parseCommand () {
     } else if (command.compare("st") == 0) {
         setTime()
         serial.writeLine("Time has been set")
+    } else if (command.compare("up") == 0) {
+        upLoadUSB()
     } else {
         serial.writeLine("Invalid command")
     }
 }
 function dec2bin (num: number) {
+    string = ""
     maxbits = 9
     maxbits += -1
     x = num
@@ -32,12 +35,18 @@ function leadingZero (num: number) {
         return convertToText(num)
     }
 }
+bluetooth.onBluetoothConnected(function () {
+    connected = 1
+    upLoad()
+})
+bluetooth.onBluetoothDisconnected(function () {
+    connected = 0
+})
 function upLoad () {
     basic.pause(100)
     readingsLength = dateTimeList.length
     if (readingsLength != 0) {
         for (let index = 0; index <= readingsLength - 1; index++) {
-            let connected = 0
             if (connected == 1) {
                 pinReading = dec2bin(pinReadingList[index])
                 bluetooth.uartWriteString(dateTimeList[index])
@@ -54,13 +63,12 @@ function upLoad () {
                 basic.pause(100)
             }
         }
-        basic.showIcon(IconNames.Yes)
     } else {
         bluetooth.uartWriteLine("No stored readings!")
     }
 }
 function dateTimeString () {
-    return "" + leadingZero(DS3231.date()) + "/" + leadingZero(DS3231.month()) + "/" + DS3231.year() + " " + leadingZero(DS3231.hour()) + ":" + leadingZero(DS3231.minute())
+    return "" + leadingZero(DS3231.date()) + "/" + leadingZero(DS3231.month()) + "/" + DS3231.year() + " " + leadingZero(DS3231.hour()) + ":" + leadingZero(DS3231.minute()) + ":" + leadingZero(DS3231.second())
 }
 function setTime () {
     serial.writeLine("" + command + ": " + params)
@@ -80,6 +88,30 @@ function setTime () {
     )
     serial.writeLine("Date & time have been set ")
 }
+function upLoadUSB () {
+    readingsLength = dateTimeList.length
+    if (readingsLength != 0) {
+        for (let index = 0; index <= readingsLength - 1; index++) {
+            // convert number from pins to binary string
+            pinReading = dec2bin(pinReadingList[index])
+            serial.writeString(dateTimeList[index])
+            basic.pause(10)
+            serial.writeString(", ")
+            basic.pause(10)
+            for (let bit2 = 0; bit2 <= 4; bit2++) {
+                serial.writeString("" + pinReading.charAt(8 - bit2) + ",")
+                basic.pause(10)
+            }
+            for (let bit3 = 0; bit3 <= 3; bit3++) {
+                serial.writeString("" + pinReading.charAt(3 - bit3) + ",")
+                basic.pause(10)
+            }
+            serial.writeLine("")
+        }
+    } else {
+        serial.writeLine("No stored readings!")
+    }
+}
 let inNumber = 0
 let charIn = ""
 let mm = ""
@@ -89,18 +121,22 @@ let mo = ""
 let yr = ""
 let pinReading = ""
 let readingsLength = 0
-let string = ""
+let connected = 0
 let bit = 0
 let div = 0
 let x = 0
 let maxbits = 0
+let string = ""
 let params = ""
 let stringIn = ""
 let command = ""
 let pinReadingList: number[] = []
 let dateTimeList: string[] = []
+serial.writeLine("Starting DATA LOGGER!")
+// Turn off display to allow use of all pins
+led.enable(false)
 let Pstate = [0]
-let sampleTime = 5000
+let sampleTime = 1000
 pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
@@ -140,7 +176,7 @@ loops.everyInterval(sampleTime, function () {
         inNumber += 256 * pins.digitalReadPin(DigitalPin.P9)
     }
     if (inNumber != lastInNumber) {
-        serial.writeLine("Writing change to log: " + inNumber)
+        serial.writeLine("Logging: " + inNumber)
         // store pin state
         pinReadingList.push(inNumber)
         // store timestamp
